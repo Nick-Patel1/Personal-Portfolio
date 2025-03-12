098b19de31a25fc73dd60f623e77c6f44f0d0edf
Nick-Patel1 <105527171+Nick-Patel1@users.noreply.github.com>
2025-03-12 22:41:06 +0000

    edit website

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-var currentDate = new Date();
-var dateTimeString = "Today's Date and Time: " + currentDate.toLocaleString();
-document.getElementById("datetime").innerHTML = dateTimeString;
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
